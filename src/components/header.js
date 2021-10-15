c22db17f2c2cdb24f674b9b7d85481f6441de999
ismailbilal <is123bilal@gmail.com>
2021-10-16 00:04:08 +0100

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { HeaderStyled, Logo, Menu, MenuLogo, LoginStyled, SearchStyled, SearchLogo, SearchInput } from "./styles/headerStyled";
+
+const Header = () => {
+    return (
+        <HeaderStyled>
+            <Logo />
+            <Menu>
+                <MenuLogo />
+                <ul>
+                    <li>Home</li>
+                    <li>Genre</li>
+                    <li>Movies</li>
+                    <li>Tv-Series</li>
+                    <li>Top IMDb</li>
+                </ul>
+            </Menu>
+            <SearchStyled>
+                <SearchLogo children='srh' />
+                <SearchInput />
+            </SearchStyled>
+            <LoginStyled />
+        </HeaderStyled>
+    )
+}
+
+export default Header;
---
+++ src/components/styles/headerStyled.js
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+import darkMenuIcon from "./../../images/darkMenu.png";
+import lightMenuIcon from './../../images/lightMenu.png';
+import logoIcon from './../../images/logo.svg';
+import loginIcon from './../../images/login.svg';
+import loginIconHover from './../../images/loginHover.svg';
+
+const favoritColor = '#ff6600';
+
+const HeaderStyled = styled.div`
+    border: 2px solid tomato;
+    height: 70px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    `
+
+const MenuLogo = styled.div`
+    width: calc(1vw + 30px);
+    height: calc(1vw + 30px);
+    background:url(${darkMenuIcon});
+    background-size: 100% 100%;
+    cursor: pointer;
+    &:hover{
+        background: url(${lightMenuIcon});
+        background-size: 100% 100%;
+    }
+    display: none;
+    `
+
+const Logo = styled.div`
+    /* color is #ff6600 */
+    height: calc(1vw + 30px);
+    width: calc(6vw + 90px);
+    background: url(${logoIcon});
+    background-size: 100% 100%;
+    cursor: pointer;
+    margin-left: 2vw;
+    order: 2;
+    
+    
+    `
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
+    margin-left: 2vw;
+    order: 2;
+    & ul{
+        display: flex;
+        flex-direction: row;
+        gap: 2vw;
+        & li{
+            list-style: none;
+            font-size: calc(15px + .2vw);
+            color: rgba(255, 255 , 255, 0.55);
+            cursor: pointer;
+            &:hover{
+                color: ${favoritColor};
+            }
+        }
+    }
+    @media (max-width: 768px){
+        order: 1;
+        & ul{
+            display: none;
+            position: absolute;
+        }
+        & div{
+            display: block;
+        }
+        :is(&:hover, &:focus-within) ul{
+            display: flex;
+            flex-direction: column;
+            top: 70px;
+        }
+    }
+`
+
+const LoginStyled = styled.div`
+
+    background: url(${loginIcon});
+    background-size: 100% 100%;
+    width: calc(1vw + 30px);
+    height: calc(1vw + 30px);
+    margin: 0 2vw;
+    order: 5;
+    &:hover{
+        background: url(${loginIconHover});
+        background-size: 100% 100%;
+    }
+`
+
+const SearchStyled = styled.div`
+    border: 1px solid tomato;
+    order: 4;
+    margin-left: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    position: relative;
+    @media (max-width: 768px){
+        & div{
+            display: block;
+        }
+        & input{
+            display: none;
+        }
+        &:hover,
+        &:focus-within{
+            flex-direction: column;
+            & input{
+                display: block;
+                top: 50px;
+            }
+        }
+    }
+`
+
+const SearchLogo = styled.div`
+    width: calc(1vw + 25px);
+    height: calc(1vw + 25px);
+    background-color: rosybrown;
+    align-self: center;
+    display: none;
+`
+
+const SearchInput = styled.input`
+    height: calc(1vw + 25px);
+    width: calc(4vw + 180px);
+    border-radius: 50px;
+    background-color: rgba(255, 255, 255, 0.30);
+    align-self: center;
+    border: none;
+    outline: none;
+    padding: 1rem;
+    position: absolute;
+    right: 0;
+`
+
+export { Menu, HeaderStyled, MenuLogo, Logo, LoginStyled, SearchStyled, SearchInput, SearchLogo }
