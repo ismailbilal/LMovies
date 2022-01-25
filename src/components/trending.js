1aa399bad0a23eda1332bf1865d99f9d8310946d
ismailbilal <is123bilal@gmail.com>
2022-01-25 15:28:21 +0100

    add movie info

--- src/components/trending.js
+++ src/components/trending.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { getTrending } from '../API';
-import { Dots, Dot, Main } from './styles/trendingStyled'
+import { getItem, getTrending } from '../API';
+import { Dots, Dot, Main, Info } from './styles/trendingStyled'
 import TrendingMovie from './trendingMovie';
 
 function Trending() {
     const [list, setList] = useState([]);
+    const [InfoList, setInfoList] = useState([]);
+    const [genres, setGenres] = useState([]);
+
     const dots = [];
+
     for (let i = 0; i < 9; i++) {
         dots.push(<Dot key={i} className='dot' />);
     }
@@ -13,8 +17,13 @@ function Trending() {
     useEffect(() => {
         const fetchData = async () => {
             const movies = await getTrending();
-            setList(movies)
-            console.log(movies);
+            for (let i = 0; i < 9; i++) {
+                const movieInfo = await getItem(movies[i].media_type, movies[i].id);
+                setInfoList(prev => [...prev, movieInfo]);
+                setGenres(prev => [...prev, movieInfo.genres]);
+                console.log(movieInfo);
+            }
+            setList(movies);
         }
 
         fetchData();
@@ -23,8 +32,27 @@ function Trending() {
     return (
         <Main>
             {
-                list.map((movie, key) => {
+                list.slice(0, 9).map((movie, key) => {
                     return <TrendingMovie key={key} movie={movie} index={key} />
+                })
+            }
+            {
+                InfoList.map((movie, index) => {
+                    return <Info key={index} className='info' >
+                        <h2>{movie.original_title || movie.title}</h2>
+                        <span>
+                            <em><div></div> {movie.vote_average}</em>
+                            <em>{movie.runtime} min</em>
+                            <em>
+                                {
+                                    genres.map((genre, index) => {
+                                        return <em key={index}>{genre.name}</em>
+                                    })
+                                }
+                            </em>
+                        </span>
+                        <p>{movie.overview}</p>
+                    </Info>
                 })
             }
             <Dots>{dots}</Dots>
