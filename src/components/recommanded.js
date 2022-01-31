eb6d084d5d87d44d2a34bba6351cc066bfeb7957
ismailbilal <is123bilal@gmail.com>
2022-01-31 03:17:09 +0100

    Add switch feature between movie, tv and trending in recommended component

--- src/components/recommanded.js
+++ src/components/recommanded.js
@@ -2,27 +2,53 @@ import React, { useEffect, useState } from 'react'
 import { List, Movie, RecHeader, RecommandedStyled, TypesStyled } from './styles/rcdStyled'
 import { getDiscover, getTrending, IMGPATH } from '../API';
 import ItemInfo from './itemInfo';
+import { render } from '@testing-library/react';
 
 const Recommanded = () => {
     const [moviesList, setMoviesList] = useState([]);
     const [type, setType] = useState('movie');
     const [page, setPage] = useState(1);
 
-    const changeType = (element, type) => {
-        const oldElement = document.querySelector('.selected');
-        oldElement.classList.remove('selected');
-        oldElement.classList.add('hoverEvent')
-        element.classList.remove('hoverEvent');
-        element.classList.add('selected');
+    const fetchMovies = async () => {
+        document.querySelector('.selected').classList.add('hoverEvent');
+        document.querySelector('.selected').classList.remove('selected');
+        document.querySelector('.movie').classList.remove('hoverEvent');
+        document.querySelector('.movie').classList.add('selected');
+        const list = await getDiscover('movie', 1);
+        const list2 = await getDiscover('movie', 2);
+        setMoviesList(list);
+        setMoviesList(prev => [...prev, ...list2]);
+        setType('movie');
     }
 
-    const fetchData = async () => {
-        const list = await getDiscover(type, page);
+    const fetchTv = async () => {
+        document.querySelector('.selected').classList.add('hoverEvent');
+        document.querySelector('.selected').classList.remove('selected');
+        document.querySelector('.tv').classList.remove('hoverEvent');
+        document.querySelector('.tv').classList.add('selected');
+        const list = await getDiscover('tv', 1);
+        const list2 = await getDiscover('tv', 2);
         setMoviesList(list);
+        setMoviesList(prev => [...prev, ...list2]);
+        setType('tv');
+    }
+
+    const fetchTrending = async () => {
+        document.querySelector('.selected').classList.add('hoverEvent');
+        document.querySelector('.selected').classList.remove('selected');
+        document.querySelector('.trend').classList.remove('hoverEvent');
+        document.querySelector('.trend').classList.add('selected');
+        const list = await getTrending('all', 'day', 1);
+        const list2 = await getTrending('all', 'day', 2);
+        setMoviesList(list);
+        setMoviesList(prev => [...prev, ...list2]);
     }
 
     useEffect(() => {
-        fetchData();
+        document.querySelector('.movie').classList.add('selected');
+        document.querySelector('.tv').classList.add('hoverEvent');
+        document.querySelector('.trend').classList.add('hoverEvent');
+        fetchMovies();
     }, [])
 
     return (
@@ -30,15 +56,15 @@ const Recommanded = () => {
             <RecHeader>
                 <p>Recommanded</p>
                 <TypesStyled>
-                    <button onClick={(event) => changeType(event.target, 'multi')} className='selected'>
+                    <button onClick={fetchMovies} className='movie'>
                         <i className="fas fa-video"></i>
                         Movies
                     </button>
-                    <button onClick={(event) => changeType(event.target, 'multi')} className='hoverEvent'>
+                    <button onClick={fetchTv} className='tv'>
                         <i className="fas fa-list-ul"></i>
                         TV Show
                     </button>
-                    <button onClick={(event) => changeType(event.target, 'multi')} className='hoverEvent'>
+                    <button onClick={fetchTrending} className='trend'>
                         <i className="fas fa-chart-line"></i>
                         Trinding
                     </button>
