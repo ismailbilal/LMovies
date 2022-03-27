ab70023c160c0f82203d2147daabab655647fd77
ismailbilal <is123bilal@gmail.com>
2022-03-27 16:24:56 +0000

    fix a bug in router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/header'
-import Home from './components/home'
-import Movie from './components/movie'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./components/home";
+import Movie from "./components/movie";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/LMovies">
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/:type/:id" element={<Movie />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
